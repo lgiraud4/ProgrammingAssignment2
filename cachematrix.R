1edc3f005eeeb7f45343ff72c90fa3554dbe48e5
Laurent GIRAUD, PhD <lgiraud4@users.noreply.github.com>
2015-04-29 22:59:37 +0200

    makecachematrix and cachesolve testée et validée

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+  ## x une matrice carree inversible
+  ## retourne une liste avec les fonctions
+  ##               set  matrix, get  matrix, set  inverse, get  inverse
+  ##         cette liste est un input à chacheSolve()
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  
+  inv1 <- NULL
+  set <- function(y) {
+    x <<- y
+    inv1 <<- NULL
+  }
+  get<- function() x
+  setinverse <- function(inverse) inv1 <<- inverse 
+  getinverse <- function() inv1
+  list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
-
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## x: output de makeCacheMatrix()
+  ## retourne: l'inverse de la matrice en input de makeCacheMatrix()
+  
+  inv1 <- x$getinverse()
+  
+  # si deja calculé
+  if (!is.null(inv1)){
+    # prends le dans le cache et évite les calculs. 
+    message("getting cached data")
+    return(inv1)
+  }
+  
+  # sinon calcul l'inverse
+  matrice.data <- x$get()
+  inv1 <- solve(matrice.data, ...)
+  
+  #met la valeur dans la liste initiale: en cache via la fonction setinverse 
+  
+  x$setinverse(inv1)
+  
+  return(inv1)
 }
+
