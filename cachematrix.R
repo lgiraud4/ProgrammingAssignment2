c2952327cb87a71a767a9757d2c09620d2375c9d
Laurent GIRAUD, PhD <lgiraud4@users.noreply.github.com>
2015-04-29 23:01:11 +0200

    second update cache matrix

--- cachematrix.R
+++ cachematrix.R
@@ -4,8 +4,7 @@
   ##         cette liste est un input à chacheSolve()
 
 makeCacheMatrix <- function(x = matrix()) {
-  
-  
+ 
   inv1 <- NULL
   set <- function(y) {
     x <<- y
@@ -16,11 +15,9 @@ makeCacheMatrix <- function(x = matrix()) {
   getinverse <- function() inv1
   list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
-
-cacheSolve <- function(x, ...) {
   ## x: output de makeCacheMatrix()
   ## retourne: l'inverse de la matrice en input de makeCacheMatrix()
-  
+cacheSolve <- function(x, ...) {
   inv1 <- x$getinverse()
   
   # si deja calculé
@@ -34,7 +31,7 @@ cacheSolve <- function(x, ...) {
   matrice.data <- x$get()
   inv1 <- solve(matrice.data, ...)
   
-  #met la valeur dans la liste initiale: en cache via la fonction setinverse 
+  #met la valeur dans la liste initiale: en "cache" via la fonction setinverse 
   
   x$setinverse(inv1)
   
